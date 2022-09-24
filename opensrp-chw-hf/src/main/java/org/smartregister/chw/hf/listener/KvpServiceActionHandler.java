5ebd6ec7ed104f6713d2a3a72f2c918583374c3a
zk497 <zameerkh12377@gmail.com>
2022-09-24 15:06:08 +0300

    Implement: KVP behavioral services actions

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/listener/KvpServiceActionHandler.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/listener/KvpServiceActionHandler.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
 
+import org.smartregister.chw.hf.activity.KvpBehavioralServiceActivity;
 import org.smartregister.chw.hf.activity.KvpBioMedicalServiceActivity;
 import org.smartregister.chw.kvp.domain.ServiceCard;
 import org.smartregister.chw.kvp.handlers.BaseServiceActionHandler;
@@ -15,6 +16,10 @@ public class KvpServiceActionHandler extends BaseServiceActionHandler {
         boolean isEditMode = isEditMode(serviceCard.getServiceEventName(), baseEntityId);
         if (serviceCard.getServiceId().equals(Constants.SERVICES.KVP_BIO_MEDICAL)) {
             KvpBioMedicalServiceActivity.startKvpBioMedicalServiceActivity((Activity) context, baseEntityId, isEditMode);
+            return;
+        }
+        else if (serviceCard.getServiceId().equals(Constants.SERVICES.KVP_BEHAVIORAL)) {
+            KvpBehavioralServiceActivity.startKvpBehavioralServiceActivity((Activity) context, baseEntityId, isEditMode);
             return;
         }
         Toast.makeText(context, serviceCard.getServiceName() + "Loading activity... ", Toast.LENGTH_SHORT).show();
