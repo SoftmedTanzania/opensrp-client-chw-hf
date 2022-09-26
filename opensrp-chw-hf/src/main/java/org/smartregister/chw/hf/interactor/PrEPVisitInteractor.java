a5012196df232fe87bf59162c8baec1cfda29c1d
Billy <billypatrick767@gmail.com>
2022-09-26 15:54:39 +0300

    added: actions for prep visit with the action helpers

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/PrEPVisitInteractor.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/PrEPVisitInteractor.java
@@ -2,6 +2,9 @@ package org.smartregister.chw.hf.interactor;
 
 import org.apache.commons.lang3.StringUtils;
 import org.smartregister.chw.hf.R;
+import org.smartregister.chw.hf.actionhelper.prep.PrEPInitiationActionHelper;
+import org.smartregister.chw.hf.actionhelper.prep.PrEPOtherServicesActionHelper;
+import org.smartregister.chw.hf.actionhelper.prep.PrEPScreeningActionHelper;
 import org.smartregister.chw.hf.actionhelper.prep.PrEPVisitTypeActionHelper;
 import org.smartregister.chw.kvp.contract.BaseKvpVisitContract;
 import org.smartregister.chw.kvp.domain.VisitDetail;
@@ -35,6 +38,10 @@ public class PrEPVisitInteractor extends BaseKvpVisitInteractor {
         final Runnable runnable = () -> {
             try {
                 evaluateVisitType(details);
+                evaluatePrEPScreening(details);
+                evaluatePrEPInitiation(details);
+                evaluateOtherServices(details);
+
             } catch (BaseKvpVisitAction.ValidationException e) {
                 Timber.e(e);
             }
@@ -56,6 +63,45 @@ public class PrEPVisitInteractor extends BaseKvpVisitInteractor {
                 .build();
 
         actionList.put(context.getString(R.string.prep_visit_type), action);
+    }
+
+    private void evaluatePrEPScreening(Map<String, List<VisitDetail>> details) throws BaseKvpVisitAction.ValidationException {
+
+        PrEPScreeningActionHelper actionHelper = new PrEPScreeningActionHelper();
+        BaseKvpVisitAction action = getBuilder(context.getString(R.string.prep_screening))
+                .withOptional(false)
+                .withDetails(details)
+                .withHelper(actionHelper)
+                .withFormName(Constants.PrEP_FOLLOWUP_FORMS.SCREENING)
+                .build();
+
+        actionList.put(context.getString(R.string.prep_screening), action);
+    }
+
+    private void evaluatePrEPInitiation(Map<String, List<VisitDetail>> details) throws BaseKvpVisitAction.ValidationException {
+
+        PrEPInitiationActionHelper actionHelper = new PrEPInitiationActionHelper();
+        BaseKvpVisitAction action = getBuilder(context.getString(R.string.prep_initiation))
+                .withOptional(false)
+                .withDetails(details)
+                .withHelper(actionHelper)
+                .withFormName(Constants.PrEP_FOLLOWUP_FORMS.INITIATION)
+                .build();
+
+        actionList.put(context.getString(R.string.prep_initiation), action);
+    }
+
+    private void evaluateOtherServices(Map<String, List<VisitDetail>> details) throws BaseKvpVisitAction.ValidationException {
+
+        PrEPOtherServicesActionHelper actionHelper = new PrEPOtherServicesActionHelper();
+        BaseKvpVisitAction action = getBuilder(context.getString(R.string.other_services))
+                .withOptional(false)
+                .withDetails(details)
+                .withHelper(actionHelper)
+                .withFormName(Constants.PrEP_FOLLOWUP_FORMS.OTHER_SERVICES)
+                .build();
+
+        actionList.put(context.getString(R.string.other_services), action);
     }
 
     @Override
